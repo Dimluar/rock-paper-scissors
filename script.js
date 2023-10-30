108698173a37e64c800757b2f0670f0e719353f2
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-10-30 15:18:42 +0100

    Plan script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Get player choice:
+// Ask the player for their choice
+// Transform choice to lower case
+// Save choice in variable
+
+// Get computer choice:
+// Select random choice from options
+// Save choice in variable
+
+// Count points:
+// Make variables for player and computer points
+// If won the match points plus one
+// If points get to three end game
+
+// Compare both choices:
+// Same choice => tie
+// Rock beats scissors
+// Paper beats rock
+// Scissors beast paper
