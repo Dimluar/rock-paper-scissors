65231fc264e98c5a8949d4ff63f6a4a05cff502b
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-10-30 18:41:52 +0100

    Get player choice

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Get player choice:
-// Ask the player for their choice
-// Transform choice to lower case
-// Save choice in variable
+function getPlayerChoice() {
+  // Ask the player for their choice
+  let playerSelection = prompt("Your choice?");
+  // Transform choice to lower case
+  playerSelection = playerSelection.toLowerCase();
+  // Make sure it's a valid choice
+  switch (playerSelection) {
+    case "rock":
+    case "paper":
+    case "scissors":
+      break;
+    default:
+      console.log("Choose a valid move.");
+      getPlayerChoice();
+  }
+  // return choice
+  return playerSelection;
+}
 
 // Get computer choice:
 function getComputerChoice() {
@@ -9,7 +24,7 @@ function getComputerChoice() {
   const computerSelection = ["rock", "paper", "scissors"];
   // Select random choice from options
   let selector = Math.floor(Math.random() * 3);
-  // Save choice in variable
+  // return choice
   return computerSelection[selector];
 }
 
